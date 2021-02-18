a7bec285b4fdca7cfb1ebfe9b5887b96b7831c7b
ocean1229 <ocean1229@naver.com>
2021-02-18 20:24:57 +0900

    create solution

--- day01/Day01/Day01/day0802.cpp
+++ day01/Day01/Day01/day0802.cpp
@@ -1,20 +1,71 @@
 #include<stdio.h>
-#include<malloc.h>
 
-typedef struct Room {
-    int in, out;
-}Rm;
+typedef struct WelcomeHotel {
+    int roomSu, roomNumber, hotel[5];
+}WH;
 
-int input(const char* msg); //정수를 입력받는 함수
+void made_room(void) {
+    for (int i = 0; i < 5; i++)
+    {
 
-int madeRoom(const char* msg) {
-    int nullRoom;
+    }
+}
+
+void in_room() {
+
+}
+
+void out_room() {
+    
+}
+
+void see_room() {
+
+}
+
+int input(const char* msg) {
+    int roomSu;
     printf("%s : ", msg);
-    scanf("%d", &nullRoom);
-                                            //TODO 방관리 프로그램 만들기
-    return nullRoom;
+    scanf_s("%d", &roomSu);
+
+    return roomSu; 
+}
+
+void select(){
+    int menu_select;
+    menu_select = input("1.입실 2.퇴실 3.보기 4.종료 : ");
+    scanf_s("%d", &menu_select);
+
+    return menu_select;
 }
 
 int main(void) {
 
+    printf("방 관리 프로그램\n");
+
+    while (1) {
+        printf("\n방의 갯수는 : %d\n",roomSu);
+
+        select();
+
+        switch (menu_select) {
+        case '1':
+            in_room();
+            break;
+
+        case '2':
+            out_room();
+            break;
+
+        case '3':
+            see_room(); 
+            break;
+
+        case '4':
+            return 0;
+
+        default:
+            printf("잘못 입력하셨습니다\n");
+        }
+    }
 }
