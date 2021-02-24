50556c2d6cc61af7c83388bfa0c620e5e70a6743
ocean1229 <ocean1229@naver.com>
2021-02-24 21:46:53 +0900

    create solution

--- stack/Project1/Project1/linkedlist.cpp
+++ stack/Project1/Project1/linkedlist.cpp
@@ -15,18 +15,18 @@ void pop(Stack** top); //데이터 삭제 함수
 void peek(Stack** top);//데이터 출력함수 가장위에 데이터만
 void Allpeek(Stack** top); //전체 데이터 출력
 int main(void)
-{
+{ 
     Stack* top = NULL;
     push(&top, 10);
-    /* peek(&top); */
-    /* push(&top, 20);
+    peek(&top);
+    push(&top, 20);
     peek(&top);
     push(&top, 30);
     Allpeek(&top);
     pop(&top);
     pop(&top);
     pop(&top);
-    pop(&top); */
+    pop(&top); 
     return 0; 
 }
 
@@ -39,25 +39,22 @@ Stack* getNode()
 
 void push(Stack** top, int data)
 {
-    
-    printf("%d\n", data);                               //data = 10
-    printf("%d\n", *top);                               //top = 0
-    Stack* tmp = *top;                                  //tmp = top의 주소로 간다 
-    printf("%d\n", data);                               //data = 10
-    printf("%d\n", *top);                               //top = 0
-    printf("%d\n", tmp);                                //tmp = 0
+/*    Stack* tmp = *top;                                  //tmp = top의 주소로 간다 
     *top = getNode();                                   //node받기 
-    (*top)->data = data;                                // *top -> 10이 있는 data로 감(주소로감)
-    printf("%d\n", *top);                               // top = 주소
-    printf("%d\n", data);                               // data = 10
-    printf("%d\n", tmp);                                // tmp = 0
-    printf("%d\n", (*top)->link);
-    (*top)->link = tmp;          
-    printf("%d\n", (*top)->link);
-    printf("%d\n", (*top)->link);
+    (*top)->data = data;                                // *top -> 10이 있는 data로 감(주소로감)                       
+    (*top)->link = tmp;      
 
+    printf("%d\n", (*top)->data);*/
 
-    printf("%d\n", (*top)->data);
+    //임시 포인터 변수 tmp를 선언하면서 getNode함수의 반환값 저장
+    Stack* tmp = getNode();
+    //tmp가 가리키는 노드의 data필드에 data저장
+    tmp->data = data;
+    //tmp의 링크필드에 main의 탑이 가지고 있는 주솟값 저장
+    tmp->link = *top;
+    //main의 top에 tmp가 가지고 있는 주솟값 저장.
+    *top = tmp;
+
     return;
 }
 
