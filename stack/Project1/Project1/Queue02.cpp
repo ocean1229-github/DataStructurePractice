77924f005ad4e774e1a01ad91dfcc7be347d1edd
ocean1229 <ocean1229@naver.com>
2021-03-02 23:11:06 +0900

    create solution

--- stack/Project1/Project1/Queue02.cpp
+++ stack/Project1/Project1/Queue02.cpp
@@ -14,7 +14,6 @@ void enqueue(Queue** front, Queue** rear, int data);  //데이터 입력
 void dequeue(Queue** front, Queue** rear);//데이터 삭제
 void peek(Queue** front); //먼저 삭제될데이터
 void Allpeek(Queue** front); //전체 데이터 출력
-Stack* 
 
 int main(void)
 {
@@ -47,15 +46,34 @@ Queue* getNode()
 }
 
 void enqueue(Queue** front, Queue** rear, int data)
+//**front : 큐의 맨 앞쪽, rear : 큐의 맨 뒤쪽, data : 숫자데이터
 {
-    front* tmp;
-    tmp = getNode();
-    tmp->data = data;
+    Queue* tmp;                 // 구조 저장할 임시변수 선언
+    tmp = getNode();            
+    tmp->data = data;           // 입력받은 숫자 데이터를 tmp가 가르키는 data영역에 저장한다 
+    printf("data : %d\n", tmp);
+    //처음        노드 한개 보냈잖아 node끼리 연결시키고 rear를 바꾼다
+    if ((*front) == 0) {    
+        *front = *rear = tmp;
+    }
+    else {
+        *rear = (*rear)->link = tmp;
+    }
+    //두번째..
 }
 
 void dequeue(Queue** front, Queue** rear)
 {
-  
+    if ((*front) == NULL) {
+        printf("No Data");
+        return;
+    }
+    else if((*front)->link == 0){
+        Queue* tmp = *front;
+        *front = *rear = NULL;
+        free(tmp);
+        return;
+    }
 }
 
 void peek(Queue** front)
